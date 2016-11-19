44c059caa5ea125734a0b8d0c5eb335b7ccb3f83
Narrev <NarrevRBX@gmail.com>
2016-11-19 16:14:54 -0600

    Create Maid.lua

---
+++ Maid.lua
@@ -0,0 +1,53 @@
+local MakeMaid do
+	local index = {
+		GiveTask = function(self, Task)
+			local n = #self.Tasks + 1
+			self.Tasks[n] = Task
+			return n
+		end;
+		
+		DoCleaning = function(self)
+			local Tasks = self.Tasks
+			for Name, Task in next, Tasks do
+				if type(Task) == "function" then
+					Task()
+				else
+					Task:Disconnect()
+				end
+				Tasks[Name] = nil
+			end
+		end;
+	};
+	index.Disconnect = index.DoCleaning -- Allow maids to be stacked.
+
+	local mt = {
+		__index = function(self, k)
+			if index[k] then
+				return index[k]
+			else
+				return self.Tasks[k]
+			end
+		end;
+		
+		__newindex = function(self, k, v)
+			local Tasks = self.Tasks
+			if v == nil then
+				if type(Tasks[k]) ~= "function" and Tasks[k] then
+					Tasks[k]:Disconnect() -- disconnect if the task is an event
+				end
+			elseif Tasks[k] then
+				self[k] = nil -- clear previous task
+			end
+			Tasks[k] = v
+		end;
+	}
+
+	function MakeMaid()
+		return setmetatable({
+			Tasks = {};
+			Instances = {};
+		}, mt)
+	end
+end
+
+return {new = MakeMaid}
