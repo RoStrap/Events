12c00da2c4a5c7b232988641bb287bfe9ad8155c
Validark <NarrevRBX@gmail.com>
2018-04-08 22:22:00 -0600

    Update Signal.lua

--- Signal.lua
+++ Signal.lua
@@ -16,10 +16,6 @@ Addresses two flaws in previous implementations:
 		listeners/threads becomes greater than 0. The destructor is called
 		when then number of threads/listeners becomes 0. The destructor
 		receives as arguments the values returned by the constructor.
-	- event = Signal:GetEvent()
-		Get the signal's event object. Returns a SignalEvent.
-	- event = Signal.Event
-		API-compatible alias for GetEvent. Returns a SignalEvent.
 	- Signal:Fire(...)
 		Fire the signal, passing the arguments to each listener and waiting
 		threads.
@@ -36,23 +32,15 @@ Addresses two flaws in previous implementations:
 	- SignalConnection:Disconnect()
 		Disconnects the listener, causing it to no longer be called when the
 		signal is fired.
-	- bool =  SignalConnection:IsConnected()
-		Returns whether the listener is connected.
 	- bool = SignalConnection.Connected
-		API-compatible alias for IsConnected.
+		Whether the listener is connected.
 ]]
 
 local function Destruct(self)
-	if #self.Connections <= 0 and self.Destructor and self.ConstructorData then
-		self:Destructor(unpack(self.ConstructorData, 1, self.ConstructorData.n))
+	if #self.Connections == 0 and self.Destructor and self.ConstructorData then
+		self:Destructor(unpack(self.ConstructorData))
 		self.ConstructorData = nil
 	end
-end
-
-local function Pack(...)
-	local t = {...}
-	t.n = #t
-	return t
 end
 
 local PseudoConnection = {
@@ -70,7 +58,7 @@ function PseudoConnection.__index:Disconnect()
 	local Signal = self.Signal
 
 	if Signal then
-		self.Connected = false	
+		self.Connected = false
 		local Connections = Signal.Connections
 
 		for i = 1, #Connections do
@@ -81,32 +69,35 @@ function PseudoConnection.__index:Disconnect()
 		end
 
 		self.Signal = Destruct(Signal)
-	end	
+	end
 end
 
 local Signal = {
 	__index = {
 		NextId = 0; -- Holds the next Arguments ID
-		Threads = 0; -- Number of Threads waiting on the signal
-		ConstructorData = nil; -- Values returned by Constructor and passed Destructor
+		YieldingThreads = 0; -- Number of Threads waiting on the signal
 	}
 }
 
 function Signal.__index:Connect(Function)
-	if #self.Connections <= 0 and self.Constructor and not self.ConstructorData then
-		self.ConstructorData = Pack(self:Constructor())
+	if #self.Connections == 0 and self.Constructor and not self.ConstructorData then
+		self.ConstructorData = {self:Constructor()}
 	end
-	
+
 	local Connection = setmetatable({
-		Signal = self,
+		Signal = self;
 		Connection = self.Bindable.Event:Connect(function(Id)
 			local Arguments = self.Arguments[Id]
-			Arguments[1] = Arguments[1] - 1
-			if Arguments[1] <= 0 then
+			local ThreadsRemaining = Arguments[1] - 1
+
+			if ThreadsRemaining == 0 then
 				self.Arguments[Id] = nil
+			else
+				Arguments[1] = ThreadsRemaining
 			end
-			Function(unpack(Arguments[2], 1, Arguments[2].n))
-		end)
+
+			Function(unpack(Arguments, 2))
+		end);
 	}, PseudoConnection)
 
 	self.Connections[#self.Connections + 1] = Connection
@@ -116,22 +107,24 @@ end
 function Signal.__index:Fire(...)
 	local Id = self.NextId
 	self.NextId = self.NextId + 1
-	self.Arguments[Id] = {#self.Connections + self.Threads, Pack(...)}
-	self.Threads = nil
+	self.Arguments[Id] = {#self.Connections + self.YieldingThreads, ...}
+	self.YieldingThreads = nil
 	self.Bindable:Fire(Id)
 end
 
 function Signal.__index:Wait()
-	self.Threads = self.Threads + 1
+	self.YieldingThreads = self.YieldingThreads + 1
 	local Id = self.Bindable.Event:Wait()
 	local Arguments = self.Arguments[Id]
-	Arguments[1] = Arguments[1] - 1
+	local ThreadsRemaining = Arguments[1] - 1
 
-	if Arguments[1] <= 0 then
+	if Arguments[1] == 0 then
 		self.Arguments[Id] = nil
+	else
+		Arguments[1] = ThreadsRemaining
 	end
 
-	return unpack(Arguments[2], 1, Arguments[2].n)
+	return unpack(Arguments, 2)
 end
 
 function Signal.__index:Destroy()
@@ -148,7 +141,7 @@ function Signal.__index:Destroy()
 		Connections[i] = nil
 	end
 
-	self.Threads = nil
+	self.YieldingThreads = nil
 	self.Arguments = nil
 	self.Connections = nil
 	setmetatable(self, nil)
@@ -157,11 +150,12 @@ end
 function Signal.new(Constructor, Destructor)
 	return setmetatable({
 		Bindable = Instance.new("BindableEvent"); -- Dispatches scheduler-compatible Threads
-		Arguments = {}; -- Holds arguments for pending listener functions and Threads: [Id] = {#Connections, #Threads, {arguments}}
+		Arguments = {}; -- Holds arguments for pending listener functions and Threads: [Id] = {#Connections + YieldingThreads, arguments}
 		Connections = {}; -- SignalConnections connected to the signal
 		Constructor = Constructor; -- Constructor function
-		Destructor = Destructor; -- Destructor function		
+		Destructor = Destructor; -- Destructor function
 	}, Signal)
 end
 
 return Signal
+		
