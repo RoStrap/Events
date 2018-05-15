e10fedaa968b1f431ed4e1d321d4a543651f1b6a
Validark <NarrevRBX@gmail.com>
2018-05-15 02:52:26 -0500

    Remove +1 offset
    Previously, this module offset the syncing by 1 so that you wouldn't get a situation where it continuously calls `wait` with smaller and smaller numbers until it passes the threshold where it can then call `wait` with higher numbers again. Now that we use `math.ceil` for the calculations, this is no longer a problem.

--- SyncedPoller.lua
+++ SyncedPoller.lua
@@ -1,19 +1,28 @@
 -- Polling synced to os.time()
 
-local SecondsPerHour = 3600
-local HourDifference = math.floor((os.time() - tick()) / SecondsPerHour + 0.5) * SecondsPerHour
+local tick = tick
+local wait = wait
+local ceil = math.ceil
+
+local HourDifference do
+	-- Get the difference in seconds between os.time() and tick(), rounded to the nearest hour
+
+	local SecondsPerHour = 3600
+	HourDifference = math.floor((os.time() - tick()) / SecondsPerHour + 0.5) * SecondsPerHour
+end
 
 local SyncedPoller = {}
 
 function SyncedPoller.new(Interval, Func)
 	-- Calls Func every Interval seconds
 	-- @param number Interval How often in seconds Func() should be called
+	--	Obviously this uses `wait`, so 0 is a valid interval but it will in reality be about (1 / 30)
 	-- @param function Func the function to call
 
 	spawn(function()
 		while true do
 			local t = tick() + HourDifference
-			wait(math.ceil(t / Interval) * Interval + 1 - t)
+			wait(ceil(t / Interval) * Interval - t)
 			Func()
 		end
 	end)
